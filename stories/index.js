8a6cdf1fe182d5397890eeb5b8835ccc5af9718b
Rebecca Chen <rebecca061996@gmail.com>
2021-02-15 18:37:17 -0500

    add story for status component

--- stories/index.js
+++ stories/index.js
@@ -15,6 +15,7 @@ import Header from 'components/Appointment/Header';
 import Empty from 'components/Appointment/Empty';
 import Show from 'components/Appointment/Show';
 import Confirm from 'components/Appointment/Confirm';
+import Status from 'components/Appointment/Status';
 
 storiesOf("Button", module)
   .addParameters({
@@ -142,3 +143,4 @@ storiesOf("Appointment", module)
   .add("Empty", () => <Empty onAdd={action("onAdd")} />)
   .add("Show", () => <Show student="Lydia Miller-Jones" interviewer={interviewer} onEdit={action("onEdit")} onDelete={action("onDelete")} />)
   .add("Confirm", () => <Confirm message="Delete the appointment?" onConfirm={action("onConfirm")} onCancel={action("onCancel")} />)
+  .add("Status", () => <Status message="Deleting" />)
