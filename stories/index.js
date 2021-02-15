a045dc32991ffe8983d913fd5152b2dabee262d9
Rebecca Chen <rebecca061996@gmail.com>
2021-02-15 18:16:59 -0500

    add story for Header component

--- stories/index.js
+++ stories/index.js
@@ -11,6 +11,7 @@ import DayList from 'components/DayList';
 import InterviewerListItem from 'components/InterviewerListItem';
 import InterviewerList from 'components/InterviewerList';
 import Appointment from 'components/Appointment';
+import Header from 'components/Appointment/Header';
 
 storiesOf("Button", module)
   .addParameters({
@@ -134,3 +135,4 @@ storiesOf("Appointment", module)
   })
   .add("Appointment", () => <Appointment />)
   .add("Appointment with Time", () => <Appointment time="12pm" />)
+  .add("Header", () => <Header time="12pm" />)
