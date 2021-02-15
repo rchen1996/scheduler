837a3ef376ab956eb4e7f3d5d5afe5d6e89f14bf
Rebecca Chen <rebecca061996@gmail.com>
2021-02-15 14:08:49 -0500

    update day list item component to add a className based on props for styling

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import 'components/DayListItem.scss';
+import classNames from 'classnames';
 
 export default function DayListItem(props) {
+  const dayListItemClass = classNames('day-list__item', {'day-list__item--selected': props.selected, 'day-list__item--full': props.spots === 0 })
+
   return (
-    <li onClick={() => props.setDay(props.name)}>
+    <li className={dayListItemClass} onClick={() => props.setDay(props.name)}>
       <h2 className="text--regular">{props.name}</h2>
       <h3 className="text--light">{props.spots}</h3>
     </li>
