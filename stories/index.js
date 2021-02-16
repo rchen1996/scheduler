584175681b98a08588bc5f9c7f4b200be28080e6
Rebecca Chen <rebecca061996@gmail.com>
2021-02-15 19:46:21 -0500

    create story for empty appointment

--- stories/index.js
+++ stories/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Fragment } from "react";
 
 import { storiesOf } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
@@ -149,3 +149,9 @@ storiesOf("Appointment", module)
   .add("Error", () => <Error message="Could not delete appointment" onClose={action("onClose")} />)
   .add("Edit Form", () => <Form name="Lydia Miller-Jones" interviewers={interviewers} value={3} onSave={action("onSave")} onCancel={action("onCancel")} />)
   .add("Create Form", () => <Form interviewers={interviewers} onSave={action("onSave")} onCancel={action("onCancel")} />)
+  .add("Appointment Empty", () => (
+    <Fragment>
+      <Appointment id={1} time="12pm" />
+      <Appointment id="last" time="1pm" />
+    </Fragment>
+  ))
