b0519907265e1e1bdaef41194c518afd2781f7e9
Rebecca Chen <rebecca061996@gmail.com>
2021-02-15 19:07:54 -0500

    create form component

---
+++ src/components/Appointment/Form.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+import InterviewerList from 'components/InterviewerList';
+import Button from 'components/Button';
+
+export default function Form(props) {
+  return (
+    <main className="appointment__card appointment__card--create">
+      <section className="appointment__card-left">
+        <form autoComplete="off">
+          <input
+            className="appointment__create-input text--semi-bold"
+            name={props.name}
+            type="text"
+            placeholder="Enter Student Name"
+            /*
+              This must be a controlled component
+            */
+          />
+        </form>
+        <InterviewerList interviewers={props.interviewers} value={props.value} onChange={props.setInterviewer} />
+      </section>
+      <section className="appointment__card-right">
+        <section className="appointment__actions">
+          <Button danger onClick={props.onCancel}>Cancel</Button>
+          <Button confirm onClick={props.onSave}>Save</Button>
+        </section>
+      </section>
+    </main>
+  )
+}
