8ccf52854faa9549644cad525accb353674391aa
Rebecca Chen <rebecca061996@gmail.com>
2021-02-16 15:48:32 -0500

    create selector to get appointments for a day and unit tests for the function

---
+++ src/helpers/selectors.js
@@ -0,0 +1,20 @@
+export function getAppointmentsForDay(state, day) {
+  // find object in state.days array who's name matches provided day - gives access to specific day's appointment array
+  let appointments;
+  for (const dayObj of state.days) {
+    if (dayObj.name === day) {
+      appointments = [...dayObj.appointments];
+    }
+  }
+
+  if (!appointments) {
+    return [];
+  }
+  // iterate through appointment array - comparing where id matches id of states.appointments and return value
+  let dayAppointments = [];
+  for (const appointment of appointments) {
+    dayAppointments.push({...state.appointments[appointment]})
+  }
+  // if no appointments on the given day, days adata will be empty - return empty array
+  return dayAppointments;
+}
