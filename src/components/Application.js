f77279298f134eb37090d1ab219865040baf916f
Rebecca Chen <rebecca061996@gmail.com>
2021-02-16 16:06:38 -0500

    refactor to combine state

--- src/components/Application.js
+++ src/components/Application.js
@@ -57,9 +57,14 @@ const appointments = [
 ];
 
 export default function Application(props) {
-  const [day, setDay] = useState("Monday");
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+    appointments: {}
+  });
 
-  const [days, setDays] = useState([]);
+  const setDay = day => setState({ ...state, day });
+  const setDays = days => setState(prev => ({ ...prev, days }))
 
   useEffect(() => {
     axios.get(`/api/days`)
@@ -79,8 +84,8 @@ export default function Application(props) {
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
           <DayList
-            days={days}
-            day={day}
+            days={state.days}
+            day={state.day}
             setDay={setDay}
           />
         </nav>
