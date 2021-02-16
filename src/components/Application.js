d1d09102ee093b1ecd918898b90cfcada8cef750
Rebecca Chen <rebecca061996@gmail.com>
2021-02-15 20:04:50 -0500

    create test data for appointments and create schedule of application component

--- src/components/Application.js
+++ src/components/Application.js
@@ -2,6 +2,58 @@ import React, { useState } from "react";
 
 import "components/Application.scss";
 import DayList from "./DayList";
+import Appointment from './Appointment';
+
+const appointments = [
+  {
+    id: 1,
+    time: "12pm"
+  },
+  {
+    id: 2,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer: {
+        id: 1,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png"
+      }
+    }
+  }, 
+  {
+    id: 3,
+    time: "2pm",
+    interview: {
+      student: "Andie Cohen",
+      interviewer: {
+        id: 2, 
+        name: "Tori Malcolm", 
+        avatar: "https://i.imgur.com/Nmx0Qxo.png" 
+      }
+    }
+  },
+  {
+    id: 4,
+    time: "3pm"
+  },
+  {
+    id: 5,
+    time: "4pm",
+    interview: {
+      student: "Malcom Brogdan",
+      interviewer: {
+        id: 4, 
+        name: "Cohana Roy", 
+        avatar: "https://i.imgur.com/FK8V841.jpg" 
+      }
+    }
+  },
+  {
+    id: "last",
+    time: "5pm"
+  }
+];
 
 const days = [
   {
@@ -47,7 +99,9 @@ export default function Application(props) {
         />
       </section>
       <section className="schedule">
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
+        {appointments.map(appointment => (
+          <Appointment key={appointment.id} {...appointment} />
+        ))}
       </section>
     </main>
   );
