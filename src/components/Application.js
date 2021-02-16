8016c33bcc608949acccd3dfa30d12e6c1feed90
Rebecca Chen <rebecca061996@gmail.com>
2021-02-16 14:38:37 -0500

    refactor code to make a request using axios to get days rather than using hard coded days array

--- src/components/Application.js
+++ src/components/Application.js
@@ -56,26 +56,17 @@ const appointments = [
   }
 ];
 
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
-
 export default function Application(props) {
   const [day, setDay] = useState("Monday");
+
+  const [days, setDays] = useState([]);
+
+  useEffect(() => {
+    axios.get(`/api/days`)
+      .then(response => {
+        setDays(response.data)
+      })
+  }, [])
 
   return (
     <main className="layout">
