dca6dabcc654a66a276f455a7906ae21fb786af5
Rebecca Chen <rebecca061996@gmail.com>
2021-02-16 21:05:23 -0500

    implement getInterviewersForDay function selector

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -15,7 +15,7 @@ export function getAppointmentsForDay(state, day) {
   for (const appointment of appointments) {
     dayAppointments.push({ ...state.appointments[appointment] });
   }
-  // if no appointments on the given day, days adata will be empty - return empty array
+  // if no appointments on the given day, days data will be empty - return empty array
   return dayAppointments;
 }
 
@@ -33,4 +33,25 @@ export function getInterview(state, interview) {
   };
 
   return interviewObj;
+}
+
+export function getInterviewersForDay(state, day) {
+  // find object in state.days array who's name matches provided day - gives access to specific day's interviewer array
+  let interviewers;
+  for (const dayObj of state.days) {
+    if (dayObj.name === day) {
+      interviewers = [...dayObj.interviewers];
+    }
+  }
+
+  if (!interviewers) {
+    return [];
+  }
+  // iterate through interviewer array - comparing where id matches id of states.interviewers and return value
+  let dayInterviewers = [];
+  for (const interviewer of interviewers) {
+    dayInterviewers.push({ ...state.interviewers[interviewer] });
+  }
+  // if no interviewers on the given day, days data will be empty - return empty array
+  return dayInterviewers;
 }
