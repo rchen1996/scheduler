9824f8ce22d87d52c9b5411a705b9235dca5942d
Rebecca Chen <rebecca061996@gmail.com>
2021-02-17 16:59:59 -0500

    refactor useVisualMode to use state setters to alter state rather than push/pop directly on history

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -1,22 +1,27 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 
 export default function useVisualMode(initial) {
   const [mode, setMode] = useState(initial);
   const [history, setHistory] = useState([initial]);
 
   const transition = function (newMode, replace = false) {
-    if (replace) {
-      history.pop();
-    }
     setMode(newMode);
-    history.push(newMode);
+    setHistory(prev => {
+      const prevCopy = [...prev];
+      if (replace) {
+        prevCopy.pop();
+      }
+      return [...prevCopy, newMode];
+    });
   };
 
   const back = function () {
     if (history.length > 1) {
-      history.pop();
-      const last = history.length - 1;
-      setMode(history[last]);
+      const historyCopy = [...history];
+      historyCopy.pop();
+      const last = historyCopy.length - 1;
+      setMode(historyCopy[last]);
+      setHistory(historyCopy);
     }
   };
 
