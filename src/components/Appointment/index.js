28e9e4af6194a129f31c98a224d0daa6eb60d568
Rebecca Chen <rebecca061996@gmail.com>
2021-02-17 12:54:56 -0500

    create save function

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -17,6 +17,15 @@ export default function Appointment(props) {
     props.interview ? SHOW : EMPTY
   );
 
+  function save(name, interviewer) {
+    const interview = {
+      student: name,
+      interviewer,
+    };
+
+    props.bookInterview(props.id, interview);
+  }
+
   return (
     <article className="appointment">
       <Header time={props.time} />
@@ -28,7 +37,11 @@ export default function Appointment(props) {
         />
       )}
       {mode === CREATE && (
-        <Form interviewers={props.interviewers} onCancel={() => back()} />
+        <Form
+          interviewers={props.interviewers}
+          onCancel={() => back()}
+          onSave={save}
+        />
       )}
     </article>
   );
