93fb3e6011620e3917cfb9df09c4af04c107914d
Rebecca Chen <rebecca061996@gmail.com>
2021-02-17 13:16:21 -0500

    update to transition to show when the promise returned by props.bookInterview resolves

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -23,9 +23,9 @@ export default function Appointment(props) {
       interviewer
     };
 
-    props.bookInterview(props.id, interview);
-
-    transition(SHOW);
+    props.bookInterview(props.id, interview).then(() => {
+      transition(SHOW);
+    });
   }
 
   return (
