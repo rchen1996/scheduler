d5d4f440c4d6f3f96452f1b42d9a12774feab42a
Rebecca Chen <rebecca061996@gmail.com>
2021-02-16 20:55:20 -0500

    implement conditional rendering of empty, show, and create components using useVisualMode hook

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -4,19 +4,30 @@ import './styles.scss';
 import Header from './Header';
 import Show from './Show';
 import Empty from './Empty';
+import Form from './Form';
+
+import useVisualMode from '../../hooks/useVisualMode';
 
 export default function Appointment(props) {
+  const EMPTY = 'EMPTY';
+  const SHOW = 'SHOW';
+  const CREATE = 'CREATE';
+
+  const { mode, transition, back } = useVisualMode(
+    props.interview ? SHOW : EMPTY
+  );
+
   return (
     <article className="appointment">
       <Header time={props.time} />
-      {props.interview ? (
+      {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
+      {mode === SHOW && (
         <Show
           student={props.interview.student}
           interviewer={props.interview.interviewer}
         />
-      ) : (
-        <Empty />
       )}
+      {mode === CREATE && <Form interviewers={[]} onCancel={() => back()} />}
     </article>
   );
 }
