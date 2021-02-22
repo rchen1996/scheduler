f976038237f6731286a230f6327b5624501c944b
Rebecca Chen <rebecca061996@gmail.com>
2021-02-22 17:08:16 -0500

    add test id

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -5,7 +5,7 @@ import classNames from 'classnames';
 export default function DayListItem(props) {
   const dayListItemClass = classNames('day-list__item', {
     'day-list__item--selected': props.selected,
-    'day-list__item--full': props.spots === 0,
+    'day-list__item--full': props.spots === 0
   });
 
   const formatSpots = function (spots) {
@@ -19,7 +19,11 @@ export default function DayListItem(props) {
   };
 
   return (
-    <li className={dayListItemClass} onClick={() => props.setDay(props.name)}>
+    <li
+      className={dayListItemClass}
+      onClick={() => props.setDay(props.name)}
+      data-testid="day"
+    >
       <h2 className="text--regular">{props.name}</h2>
       <h3 className="text--light">{formatSpots(props.spots)}</h3>
     </li>
