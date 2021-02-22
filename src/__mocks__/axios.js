b33aa40897033724e466bbbd7820c39aa98c5c94
Rebecca Chen <rebecca061996@gmail.com>
2021-02-22 17:34:55 -0500

    add delete request to axios mock module

--- src/__mocks__/axios.js
+++ src/__mocks__/axios.js
@@ -80,11 +80,15 @@ export default {
     }
   }),
   put: jest.fn(url => {
-    if (url === '/api/appointments/1') {
-      return Promise.resolve({
-        status: 204,
-        statusText: 'No Content'
-      });
-    }
+    return Promise.resolve({
+      status: 204,
+      statusText: 'No Content'
+    });
+  }),
+  delete: jest.fn(url => {
+    return Promise.resolve({
+      status: 204,
+      statusText: 'No Content'
+    });
   })
 };
