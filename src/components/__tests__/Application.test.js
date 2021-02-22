d7f696fd1b5feca5a254dc909b7528dde89ad2a6
Rebecca Chen <rebecca061996@gmail.com>
2021-02-22 17:34:43 -0500

    add test for loading data, cancelling an interview, and updating spots

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -52,4 +52,33 @@ describe('Application', () => {
 
     expect(getByText(day, 'no spots remaining')).toBeInTheDocument();
   });
+
+  it('loads data, cancels an interview and increases the spots remaining for Monday by 1', async () => {
+    const { container } = render(<Application />);
+
+    await waitForElement(() => getByText(container, 'Archie Cohen'));
+
+    const appointment = getAllByTestId(
+      container,
+      'appointment'
+    ).find(appointment => queryByText(appointment, 'Archie Cohen'));
+
+    fireEvent.click(getByAltText(appointment, 'Delete'));
+
+    expect(
+      getByText(appointment, /are you sure you would like to delete?/i)
+    ).toBeInTheDocument();
+
+    fireEvent.click(queryByText(appointment, 'Confirm'));
+
+    expect(getByText(appointment, 'Deleting')).toBeInTheDocument();
+
+    await waitForElement(() => getByAltText(appointment, 'Add'));
+
+    const day = getAllByTestId(container, 'day').find(day =>
+      queryByText(day, 'Monday')
+    );
+
+    expect(getByText(day, '2 spots remaining')).toBeInTheDocument();
+  });
 });
