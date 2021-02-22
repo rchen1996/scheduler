d3e61e73125f005fae208cf0ec0f7332ba076122
Rebecca Chen <rebecca061996@gmail.com>
2021-02-22 12:19:12 -0500

    add validation for interviewers prop and add check before performing .map

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,19 +1,23 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+
 import 'components/InterviewerList.scss';
 import InterviewListItem from './InterviewerListItem';
 
 export default function InterviewerList(props) {
-  const interviewers = props.interviewers.map(interviewer => {
-    return (
-      <InterviewListItem
-        key={interviewer.id}
-        name={interviewer.name}
-        avatar={interviewer.avatar}
-        selected={interviewer.id === props.value}
-        setInterviewer={event => props.onChange(interviewer.id)}
-      />
-    );
-  });
+  const interviewers =
+    Array.isArray(props.interviewers) &&
+    props.interviewers.map(interviewer => {
+      return (
+        <InterviewListItem
+          key={interviewer.id}
+          name={interviewer.name}
+          avatar={interviewer.avatar}
+          selected={interviewer.id === props.value}
+          setInterviewer={event => props.onChange(interviewer.id)}
+        />
+      );
+    });
 
   return (
     <section className="interviewers">
@@ -22,3 +26,7 @@ export default function InterviewerList(props) {
     </section>
   );
 }
+
+InterviewerList.propTypes = {
+  interviewers: PropTypes.array.isRequired
+};
