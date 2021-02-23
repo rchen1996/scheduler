4871f9102eee5fecaefd9008d814b5d70c7bde99
Rebecca Chen <rebecca061996@gmail.com>
2021-02-23 13:31:05 -0500

    add test for edit interview

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -19,11 +19,18 @@ describe('Appointments', () => {
   });
 
   it('should edit an interview', () => {
-    cy.visit('/');
-
-    cy.contains('[data-testid=day]', 'Monday');
-
     cy.get('[alt=Edit]').first().click({ force: true });
+
+    cy.get('[data-testid=student-name-input]')
+      .clear()
+      .type('Lydia Miller-Jones');
+
+    cy.get('[alt="Tori Malcolm"]').click();
+
+    cy.contains('Save').click();
+
+    cy.contains('.appointment__card--show', 'Lydia Miller-Jones');
+    cy.contains('.appointment__card--show', 'Tori Malcolm');
   });
   // it('should cancel an interview', () => {});
 });
