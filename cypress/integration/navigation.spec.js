c6827fb0b77612557b13f5af40632ca2241d9ac6
Rebecca Chen <rebecca061996@gmail.com>
2021-02-23 13:12:05 -0500

    add test for booking an interview

--- cypress/integration/navigation.spec.js
+++ cypress/integration/navigation.spec.js
@@ -10,4 +10,23 @@ describe('Navigation', () => {
       .click()
       .should('have.class', 'day-list__item--selected');
   });
+
+  it('should book an interview', () => {
+    cy.visit('/');
+
+    cy.contains('[data-testid=day]', 'Monday');
+
+    cy.get('[alt=Add]').first().click();
+
+    cy.get('[placeholder="Enter Student Name"]').type('Rebecca');
+
+    cy.get('[alt="Sylvia Palmer"]').click();
+
+    cy.contains('button', 'Save').click();
+
+    cy.contains('[data-testid=appointment]', 'Rebecca');
+  });
+
+  // it('should edit an interview', () => {});
+  // it('should cancel an interview', () => {});
 });
